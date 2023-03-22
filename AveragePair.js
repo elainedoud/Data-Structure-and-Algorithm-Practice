7fdd507ddfa1c2f1bc7373a47160df0ad08035a1
Elaine <elainedoud@gmail.com>
2023-03-22 17:59:56 -0400

    Began to write out practice problem

---
+++ AveragePair.js
@@ -0,0 +1,20 @@
+function averagePair(arr, x){
+
+    let tempAvg = 0
+    let trueAvg = 0 
+
+    if (arr.length < x) return false;
+
+    
+
+
+    if (trueAvg === x) return true;
+
+
+}
+
+
+// What are we needing to complete?  We want two numbers in the array 
+// to be combined and if they equal the target number (x), then they will
+// return true.  If none of the numbers added equal the target, then 
+// return false.
