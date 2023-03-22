eef94cee08868afb7688adf5c94c2a762a0ac7a9
Elaine <elainedoud@gmail.com>
2023-03-22 13:31:06 -0400

    Corrected typo - now working

--- FrequencyCounter.js
+++ FrequencyCounter.js
@@ -10,19 +10,21 @@ function sameFrequency(x, y) {
         forX[strNum1[i]] = (forX[strNum1[i]] || 0) + 1
     }
 
-    for (let j = 0; j < y.length; j++) {
+    for (let j = 0; j < strNum2.length; j++) {
         forY[strNum2[j]] = (forY[strNum2[j]] || 0) + 1
     }
 
     for (let key in forX) {
-        if (forX[key] !== forY[key])
-            return false
-    }
+        if (forX[key] === forY[key])
+            return true
+    
+    else 
+        return false
 
-    return true
+}
 }
 
-console.log(sameFrequency(808,880))
+console.log(sameFrequency(880,880))
 
 // Use frequency counter pattern to determine if x and y have the 
 // same frequency of digits
@@ -31,6 +33,6 @@ console.log(sameFrequency(808,880))
 // Then iterate over each x to put the frequency of digits into 
 // x's object and do the same thing for y
 // Then compare them; if equal then return true; if unequal then return false
-
 //Troubleshooting; why is it returning undefined? - Because I did not put x or y in 
 // their respective objects
+// Still not working - why is it only returning false? - Typo in second for loop
