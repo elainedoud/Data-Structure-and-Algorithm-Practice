61cd04c61836631ea8452e533beb6a9464b2b725
Elaine <elainedoud@gmail.com>
2023-04-03 15:08:36 -0400

    The answer from solution page

--- MaxSubarraySum.js
+++ MaxSubarraySum.js
@@ -1,17 +1,24 @@
-function maxSubarraySum(arr, x){
-    i = 0 
-    j = 1
-let group1 = arr[i] + arr[i +1]
-let group2 = arr[j] + arr[j + 1]
+function maxSubarraySum(arr, n){
 
-    if (group1 > group2){
-        j++
-    }
-    else if (group1 < group2){
-        i = i + 2
-    }
+    if (arr.length < n) return null;
+
+  let max = 0
+  let sum = 0
+
+  for (let i = 0; i < n; i++){
+    sum += arr[i]
+    max = sum
+  }
+
+  for (let i = n; i < arr.length; i++){
+    sum += arr[i] - arr[i -n];
+        max = Math.max(max, sum);
+  }
+  return max;
 
 }
+
+
 // Prompt: Given an array of integers and a number, write a function called maxSubarraySum, 
 // which finds the maximum sum of a subarray with the length of the number passed to the function.
 // Note that a subarray must consist of consecutive elements from the original array. 
@@ -19,17 +26,3 @@ let group2 = arr[j] + arr[j + 1]
 //but [100, 300] is not.
 
 // Example: ([100, 200, 300, 400], 2) will return // 700
-
-// What we need to do is:
-//          Determine what x is
-//          Compare sums of the array to each other
-//          For example, for the above array, we would want to compare 100 + 200 to 300 + 400
-//          We need two groups of arrays at a time, whichever group is smaller will need to 
-//          slide to the next values in line
-//          Once the highest value that can be obtained is determined, we need to return that value
-
-// Let's start backwards; let's assume x is 2; let's create two groups & see if we can get their sum to 
-// appear correctly
-// Ok, that's working. Now, can we incorporate the slide?
-
-console.log(maxSubarraySum([1,5,7,2,-1,0], 2))
