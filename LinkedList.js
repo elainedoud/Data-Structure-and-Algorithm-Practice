359d904e43a1a3fac91914b8b2e9b2c04676c2c3
Elaine <elainedoud@gmail.com>
2023-06-29 17:03:18 -0400

    Added push, pop, shift, and unshift to Singly Linked List

--- LinkedList.js
+++ LinkedList.js
@@ -12,14 +12,57 @@ class SinglyLinkedList {
         this.tail = null;
     }
     push(val){
-        if (head = null){
-            head = val;
-            tail = val;
-            length += 1;
+      var newNode = new Node(val);
+      if (!this.head) {
+        this.head = newNode;
+        this.tail = this.head;
+      }
+      else {
+        this.tail.next = newNode;
+        this.tail = newNode;
+      }
+      this.length++;
+      return this;
+    }
+    pop(){
+        if (!this.head) return undefined;
+        var current = this.head;
+        var newTail = current;
+        while (current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+    shift(){
+        if (!this.head) return undefined;
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length === 0){
+            this.tail = null;
+        }
+        return currentHead;
+    }
+    unshift(val){
+        var newNode = new Node(val);
+        if (!this.head){
+            this.head = newNode;
+            this.tail = newNode;
         }
         else {
-
+            newNode.next = this.head;
+            this.head = newNode;
         }
+        this.length++;
+        return this;
     }
 }
 
