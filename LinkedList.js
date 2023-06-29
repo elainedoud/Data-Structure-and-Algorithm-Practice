8c820f2389a0e9831c2cee2b9aed46fe9de9a7eb
Elaine <elainedoud@gmail.com>
2023-06-29 13:20:51 -0400

    Beginning to put together Singly Linked List

---
+++ LinkedList.js
@@ -0,0 +1,33 @@
+class Node {
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor(){
+        this.length = o;
+        this.head = null;
+        this.tail = null;
+    }
+    push(val){
+        if (head = null){
+            head = val;
+            tail = val;
+            length += 1;
+        }
+        else {
+
+        }
+    }
+}
+
+//Function should accept value
+//Create a new node using the value
+// If there is no head, this new value will be set
+// to head and tail
+// Otherwise, set the next property on the tail to be 
+// the new node and set the tail property on the list to 
+// be the newly created node
+// Increment the length by one 
