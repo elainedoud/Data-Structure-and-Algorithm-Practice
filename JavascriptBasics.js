59ec1ec1ab211c413f8babaa9a0f1cc3231d1588
Elaine <elainedoud@gmail.com>
2023-06-30 13:59:47 -0400

    Began working on switch

---
+++ JavascriptBasics.js
@@ -0,0 +1,18 @@
+let expression = happy;
+
+    switch(expression){
+        case happy:
+            return "That's great!";
+         break;
+        case sad:
+            return "I'm sorry";
+         break;
+        default:
+            return "Hello";
+}
+
+
+// Something is wrong in the way I constructed this or 
+// what I'm trying to do here.  Recieving error saying happy
+// is not defined
+// Not yet working
